b19cfb1216c0b54be197e98c2652549bc4132460
romintomasetti <romin.tomasetti@gmail.com>
2022-03-09 21:55:17 +0000

    homework(1) : missing latex packages

--- compile.latex.sh
+++ compile.latex.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+set -ex
+
+COMMON_OPTIONS="-halt-on-error"
+
 tex_file=$1
 
 echo "> Compiling ${tex_file}"
 
-latex --shell-escape ${tex_file}
+latex    --shell-escape ${COMMON_OPTIONS} ${tex_file}
 
-pdflatex --shell-escape ${tex_file}
+pdflatex --shell-escape ${COMMON_OPTIONS} ${tex_file}
 
 echo "> Compilation done !"
