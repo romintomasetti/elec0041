9ea1391e768db34be5c0b41cff862f7e40bea33e
romintomasetti <romin.tomasetti@gmail.com>
2022-03-10 10:44:38 +0000

    homework(1) : run latex twice for references

--- compile.latex.sh
+++ compile.latex.sh
@@ -8,6 +8,8 @@ tex_file=$1
 
 echo "> Compiling ${tex_file}"
 
+# For references to appear correctly, we need to run LaTex twice.
+pdflatex --shell-escape ${COMMON_OPTIONS} ${tex_file}
 pdflatex --shell-escape ${COMMON_OPTIONS} ${tex_file}
 
 echo "> Compilation done !"
